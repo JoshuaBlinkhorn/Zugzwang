2d8d0d8ea4eb3da153a7a1bcbeb15846cff2c0d0
JoshuaBlinkhorn <joshuablinkhorn@hotmail.co.uk>
2022-06-26 22:16:16 +0100

    ZugSolutionData

--- zugzwang/test_ZugRoot.py
+++ zugzwang/test_ZugRoot.py
@@ -7,42 +7,64 @@ from zugzwang import (
     ZugRoot
 )
 
-def test_ZugRoot_init():
-    
-    last_access = datetime.date(day=1, month=1, year=2000)
-    new_remaining = 20
-    new_limit = 25
-    root_data = ZugRootData(last_access, new_remaining, new_limit)
+LAST_ACCESS = datetime.date(day=1, month=1, year=2000)
+NEW_REMAINING = 20
+NEW_LIMIT = 25
+NEW = 10
+REVIEW = 100
+INACTIVE = 20
+REACHABLE = 150
 
+@pytest.fixture
+def root():
+    root_data = ZugRootData(
+        LAST_ACCESS,
+        NEW_REMAINING,
+        NEW_LIMIT,
+        NEW,
+        REVIEW,
+        INACTIVE,
+        REACHABLE
+    )
     game = chess.pgn.Game()
     game.comment = root_data.make_comment()
-
-    root = ZugRoot(game)
-
-    assert root.last_access == last_access
-    assert root.new_remaining == new_remaining
-    assert root.new_limit == new_limit
-
-
-def test_ZugRoot_update_comment():
+    return ZugRoot(game)
     
-    last_access = datetime.date(day=1, month=1, year=2000)
-    new_remaining = 20
-    new_limit = 25
-    root_data = ZugRootData(last_access, new_remaining, new_limit)
 
-    game = chess.pgn.Game()
-    game.comment = root_data.make_comment()
+def test_ZugRoot_init(root):
+    assert root.data.last_access == LAST_ACCESS
+    assert root.data.new_remaining == NEW_REMAINING
+    assert root.data.new_limit == NEW_LIMIT
+    assert root.data.new == NEW
+    assert root.data.review == REVIEW
+    assert root.data.inactive == INACTIVE
+    assert root.data.reachable == REACHABLE
 
-    root = ZugRoot(game)
 
-    root.last_access = datetime.date(day=1, month=1, year=2001)
-    root.new_remaining = 0
-    root.new_limit = 5
-
+def test_ZugRoot_update_game_comment(root):
+    root.data.last_access = datetime.date(day=1, month=1, year=2001)
+    root.data.new_remaining = 0
+    root.data.new_limit = 5
+    root.data.new = 0
+    root.data.review = 110
+    root.data.inactive = 25
+    root.data.reachable = 155
     root.update_game_comment()
-    
-    expected_comment = 'last_access=01-01-2001;new_remaining=0;new_limit=5'    
+    expected_comment = (
+        'last_access=01-01-2001;'
+        'new_remaining=0;'
+        'new_limit=5;'
+        'new=0;'
+        'review=110;'
+        'inactive=25;'
+        'reachable=155'
+        )
     assert root.game.comment == expected_comment
+
     
+def test_update_status_counts(root):
+    root.update_status_counts()
+
     
+def test_update_status_counts(root):
+    root.reset_data()
--- zugzwang/test_ZugRootData.py
+++ zugzwang/test_ZugRootData.py
@@ -7,17 +7,42 @@ from zugzwang import (
 )
 
 def test_ZugRootData_from_comment():
-    comment = 'last_access=01-01-2000;new_remaining=20;new_limit=25'    
+    comment = (
+        'last_access=01-01-2000;'
+        'new_remaining=20;'
+        'new_limit=25;'
+        'new=100;'
+        'review=150;'
+        'inactive=50;'
+        'reachable=250'
+    )
     root_data = ZugRootData.from_comment(comment)
     assert root_data.last_access == datetime.date(day=1, month=1, year=2000)
     assert root_data.new_remaining == 20
     assert root_data.new_limit == 25
+    assert root_data.new == 100
+    assert root_data.review == 150
+    assert root_data.inactive == 50
+    assert root_data.reachable == 250
 
 def test_ZugRootData_make_comment():
-    last_access = datetime.date(day=1, month=1, year=2000)
-    new_remaining = 10
-    new_limit = 10
-    root_data = ZugRootData(last_access, new_remaining, new_limit)
-    expected_comment = 'last_access=01-01-2000;new_remaining=10;new_limit=10'
+    root_data = ZugRootData(
+        last_access = datetime.date(day=1, month=1, year=2000),
+        new_remaining = 20,
+        new_limit = 50,
+        new = 100,
+        review = 150,
+        inactive = 50,
+        reachable = 250
+    )
+    expected_comment = (
+        'last_access=01-01-2000;'
+        'new_remaining=20;'
+        'new_limit=50;'
+        'new=100;'
+        'review=150;'
+        'inactive=50;'
+        'reachable=250'
+    )
     assert root_data.make_comment() == expected_comment
     
