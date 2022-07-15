265e435bf918b47cccb8306229e7529c181bf651
JoshuaBlinkhorn <joshuablinkhorn@hotmail.co.uk>
2022-07-15 20:32:25 +0100

    restructure of code and testing of dates

--- zugzwang/dates.py
+++ zugzwang/dates.py
@@ -1,14 +1,23 @@
 import datetime
+from random import randint
 
 class ZugDates():
 
     @classmethod
-    def today(cls):
+    def _today(cls):
         return datetime.date.today()
 
     @classmethod
+    def today(cls):
+        return cls._today()
+
+    @classmethod
     def yesterday(cls):
-        return datetime.date.today() - datetime.timedelta(days=1)
+        return cls._today() - datetime.timedelta(days=1)
+
+    @classmethod
+    def tomorrow(cls):
+        return cls._today() + datetime.timedelta(days=1)
 
     @classmethod
     def due_date(
@@ -19,11 +28,10 @@ class ZugDates():
             recall_radius
     ):
         previous_diff = (current_due_date - last_study_date).days
-        radius = min(previous_diff, recall_radius)
-        diff = previous_diff * recall_radius
-        absolute_recall_date = current_due_date + datetime.timedelta(days = diff)
-        offset = randint(0, (radius*2)) - radius
-        recall_date = absolute_recall_date + datetime.timedelta(days = offset)
-        return recall_date
+        diff = previous_diff * recall_factor
+        absolute_recall_date = cls.today() + datetime.timedelta(days=diff)
+        offset = randint(-recall_radius, recall_radius)
+        recall_date = absolute_recall_date + datetime.timedelta(days=offset)
+        return recall_date if recall_date >= cls.tomorrow() else cls.tomorrow()
 
 
--- zugzwang/test_dates.py
+++ zugzwang/test_dates.py
@@ -1,16 +1,71 @@
 import pytest
+import mock
 import datetime
 
-from zugzwang import ZugDates
+from zugzwang.dates import ZugDates
 
+EPOCH = datetime.date(2000,1,1)
+
+def epoch_shift(shift: int):
+    return EPOCH + datetime.timedelta(days=shift)
+
+@pytest.fixture(autouse=True)
+def mock_today(monkeypatch):
+    def mock_today():
+        return EPOCH    
+    monkeypatch.setattr(ZugDates, '_today', mock_today)
 
 class TestZugDates:
 
-    def test_today():
-        ZugDates.today()
+    def test_today(self):
+        assert ZugDates.today() == epoch_shift(0)
 
-    def test_yesterday():
-        ZugDates.yesterday()
+    def test_yesterday(self):
+        ZugDates.yesterday() == epoch_shift(-1)
 
-    def test_calculate_due_date():
-        pass
+    def test_tomorrow(self):
+        ZugDates.tomorrow() == epoch_shift(1)
+
+    @pytest.mark.parametrize(
+        (
+            'last_study_date_offset, current_due_date_offset, recall_factor, '
+            'recall_radius, expected_offset_lower_bound, expected_offset_upper_bound'
+        ),
+        [
+            # last date t-1, recall_factor 1
+            (-1, 0, 1, 0, 1, 1),
+            (-1, 0, 1, 1, 1, 2),
+            (-1, 0, 1, 2, 1, 3),            
+            (-1, 0, 1, 10, 1, 11),
+            # last date t-1, recall_factor 2            
+            (-1, 0, 2, 0, 2, 2),
+            (-1, 0, 2, 1, 1, 3),
+            (-1, 0, 2, 2, 1, 4),
+            (-1, 0, 2, 10, 1, 12),
+            # last date t-1, recall_factor 5
+            (-1, 0, 5, 0, 5, 5),
+            (-1, 0, 5, 1, 1, 6),
+            (-1, 0, 5, 2, 1, 7),
+            (-1, 0, 5, 10, 1, 15),                                                
+            
+        ]        
+    )
+    def test_calculate_due_date(
+            self,
+            last_study_date_offset,
+            current_due_date_offset,
+            recall_factor,
+            recall_radius,
+            expected_offset_lower_bound,
+            expected_offset_upper_bound
+    ):
+        last_study_date = epoch_shift(last_study_date_offset)
+        current_due_date = epoch_shift(current_due_date_offset)
+
+        # the function is randomised; so repeat the test many times
+        for n in range(10000):
+            due_date = ZugDates.due_date(
+                last_study_date, current_due_date, recall_factor, recall_radius
+            )
+            assert epoch_shift(expected_offset_lower_bound) <= due_date
+            assert epoch_shift(expected_offset_upper_bound) >= due_date
