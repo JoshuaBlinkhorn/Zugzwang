dcd22578f91d08953ec7e2ef4abd142188302e8f
JoshuaBlinkhorn <joshuablinkhorn@hotmail.co.uk>
2022-10-08 19:49:59 +0100

    prepare PGN updated

--- prepare_pgn.py
+++ prepare_pgn.py
@@ -9,31 +9,35 @@ import chess
 from zugzwang.constants import ZugColours
 from zugzwang.chapter import ZugChapter
 from zugzwang.game import ZugRoot, ZugRootData, ZugSolutionData
+from zugzwang.tools import ZugChessTools
 
 # range over the filenames in the current directory
-pgn_files = os.listdir()
+pgn_files = sorted(os.listdir())
 for filename in pgn_files:
     
     if not filename.endswith('pgn'):
         print(f"Skipping '{filename}'")
         continue
-    
-    colour = None
-    while colour != 'w' and colour != 'b':
-        colour = input(f"Select perspective for '{filename}' [w/b]:'")
-    if colour == 'w':
+    if filename[-5] in 'wW':
         perspective = ZugColours.WHITE
-    else:
+    elif filename[-5] in 'bB':
         perspective = ZugColours.BLACK
+    else:
+        colour = None
+        while colour != 'w' and colour != 'b':
+            colour = input(f"Select perspective for '{filename}' [w/b]:'")
+        if colour == 'w':
+            perspective = ZugColours.WHITE
+        else:
+            perspective = ZugColours.BLACK
         
     with open(filename) as pgn_file:
         game = chess.pgn.read_game(pgn_file)
     root_data = ZugRootData(perspective)
-    game.comment = root_data.make_json()
+    game.comment = ZugRoot._to_square_braces(root_data.make_json())
     root = ZugRoot(game)
-    game.comment = ZugChapter._to_square_braces(game.comment)
-    for sol_node in root.solution_nodes():
-        sol_node.comment = ZugChapter._to_square_braces(ZugSolutionData().make_json())
+    for sol_node in ZugChessTools.get_solution_nodes(game, perspective):
+        sol_node.comment = ZugRoot._to_square_braces(ZugSolutionData().make_json())
 
     output_filename = filename.replace('.pgn','.chp')
     print(game, file=open(output_filename, 'w'))        
