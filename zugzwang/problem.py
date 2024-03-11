321774160b1c41fab75033cf09d7d532412051ab
JoshuaBlinkhorn <joshuablinkhorn@hotmail.co.uk>
2024-03-11 20:51:10 +0000

    working version

--- zugzwang/problem.py
+++ zugzwang/problem.py
@@ -3,26 +3,26 @@ from typing import List
 
 import chess
 
-from zugzwang.queue import QueueItem, QueueItemResult
+from zugzwang.queue import QueueItem, QueueResult
 from zugzwang.gui import ZugGUI
 
-def _present_problem(solution: chess.pgn.ChildNode, gui: ZugGUI) -> QueueItemResult:
+def _present_problem(solution: chess.pgn.ChildNode, gui: ZugGUI) -> QueueResult:
     failed = False    
 
-    while (result := _get_result(solution, gui)) == QueueItemResult.FAILURE:
+    while (result := _get_result(solution, gui)) == QueueResult.FAILURE:
         failed = True
-    if failed is True and result == QueueItemResult.SUCCESS:
-        result = QueueItemResult.FAILURE
+    if failed is True and result == QueueResult.SUCCESS:
+        result = QueueResult.FAILURE
 
     return result
 
 
-def _get_result(solution: chess.pgn.ChildNode, gui: ZugGUI) -> QueueItemResult:
+def _get_result(solution: chess.pgn.ChildNode, gui: ZugGUI) -> QueueResult:
     gui.setup_position(solution.parent.board())
     input_ = gui.get_input()
 
     if input_ == ZugGUI.QUIT:
-        return QueueItemResult.QUIT
+        return QueueResult.QUIT
         
     if not isinstance(input_, chess.Move):
         raise ValueError('Input from ZugGUI not recognised.')
@@ -30,9 +30,9 @@ def _get_result(solution: chess.pgn.ChildNode, gui: ZugGUI) -> QueueItemResult:
     if input_ == solution.move:
         gui.setup_position(solution.board())
         time.sleep(1)            
-        return QueueItemResult.SUCCESS
+        return QueueResult.SUCCESS
     else:
-        return QueueItemResult.FAILURE
+        return QueueResult.FAILURE
 
 
 class Problem(QueueItem):
@@ -40,7 +40,7 @@ class Problem(QueueItem):
     def __init__(self, solution: chess.pgn.ChildNode):
         self._solution = solution
 
-    def _present(self, gui: ZugGUI) -> QueueItemResult:
+    def play(self, gui: ZugGUI) -> QueueResult:
         gui.set_perspective(self._solution.parent.board().turn)   
         return _present_problem(self._solution, gui)
 
@@ -49,9 +49,9 @@ class Line(QueueItem):
     def __init__(self, line: List[chess.pgn.GameNode]):
         self._line = line
 
-    def _present(self, gui: ZugGUI):
+    def play(self, gui: ZugGUI) -> QueueResult:
         gui.set_perspective(self._line[0].board().turn)
         for solution in self._line[1::2]:
-            if (result := _present_problem(solution, gui)) != QueueItemResult.SUCCESS:
+            if (result := _present_problem(solution, gui)) != QueueResult.SUCCESS:
                 break
         return result
